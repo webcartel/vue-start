1840b4535ad6c3fb9df758b699a803281b972c22
webcartel <webcartel@yandex.ru>
2017-11-12 01:04:42 +0200

    Добавил кнопку меню в шапке

--- src/components/common/appHeader.vue
+++ src/components/common/appHeader.vue
@@ -1,6 +1,11 @@
 <template>
 	<div class="app-header">
-
+		<button class="menu-btn">
+			<svg fill="#fff" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+			    <path d="M0 0h24v24H0z" fill="none"/>
+			    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+			</svg>
+		</button>
 	</div>
 </template>
 
@@ -26,6 +31,13 @@ export default {
 	width: 100%;
 	height: 40px;
 	background-color: #3d8aca;
+}
+
+.menu-btn {
+	width: 40px;
+	background: none;
+	border: none;
+	outline: none;
 }
 
 </style>
