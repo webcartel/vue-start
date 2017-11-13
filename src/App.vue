9f724299064864277165e6453c68f7a76856abc6
webcartel <webcartel@yandex.ru>
2017-11-13 23:46:43 +0200

    Общий промежуточный коммит

--- src/App.vue
+++ src/App.vue
@@ -17,16 +17,21 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 * {
   margin: 0;
   padding: 0;
+  box-sizing: border-box;
 }
 #app {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
--- src/components/pages/index.vue
+++ src/components/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="index">
-		
+		<h1>Главная</h1>
 	</div>
 </template>
 
@@ -16,5 +16,10 @@ export default {
 </script>
 
 <style scoped>
-
+	h1 {
+		padding: 10px;
+		font: 500 14px/14px 'Roboto', sans-serif;
+		color: #37474F;
+		border-bottom: 1px solid #CFD8DC;
+	}
 </style>
